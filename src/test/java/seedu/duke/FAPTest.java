954a34308935dcec6a7c427f683cfd2a5b997bb1
Tan Dexter <dextboy@outlook.com>
2024-03-31 15:25:59 +0800

    Enhance FAPTest and update UI tests for UserStorage functionality
    - Updated FAPTest to backup and restore user info, ensuring tests do not alter application state
    - Adjusted EXPECTED.TXT to match new expected outputs reflecting changes in user information handling
    - Added a new data file (CS2113_AY2324S2_FAP_Storage.txt) under text-ui-test to simulate user data storage for testing
    - Modified input.txt in text-ui-test to align with the new testing setup and scenarios

--- src/test/java/seedu/duke/FAPTest.java
+++ src/test/java/seedu/duke/FAPTest.java
@@ -6,28 +6,43 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FAPTest {
 
+    public static Path userInfoPath = Paths.get(System.getProperty("user.dir"),
+            "data", "CS2113_AY2324S2_FAP_Storage.txt");
+    private String originalUserInfo;
     private final InputStream systemIn = System.in;
     private final PrintStream systemOut = System.out;
     private ByteArrayInputStream testIn;
     private ByteArrayOutputStream testOut;
 
     @BeforeEach
-    public void setUpStreams() {
+    public void setUpStreams() throws IOException {
         testOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(testOut));
+        if (Files.exists(userInfoPath)) {
+            originalUserInfo = Files.readString(userInfoPath);
+        }
     }
 
     @AfterEach
-    public void restoreStreams() {
+    public void restoreStreams() throws IOException {
         System.setIn(systemIn);
         System.setOut(systemOut);
+        if (originalUserInfo != null) {
+            Files.writeString(userInfoPath, originalUserInfo, StandardOpenOption.TRUNCATE_EXISTING);
+        }
     }
 
     private void provideInput(String data) {
