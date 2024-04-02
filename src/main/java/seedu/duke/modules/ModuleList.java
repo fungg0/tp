377eedf0d878590756af0645f958561cddb1ef16
fungg0 <fungfunwenwu@gmail.com>
2024-04-02 18:50:05 +0800

    Fix miscalculation with desiredGPA

--- src/main/java/seedu/duke/modules/ModuleList.java
+++ src/main/java/seedu/duke/modules/ModuleList.java
@@ -203,7 +203,7 @@ public class ModuleList {
             upperBoundGradeNeeded += 1;
             mockGPA = calculateMockGPA(upperBound, upperBoundGradeNeeded, lowerBound, lowerBoundGradeNeeded);
         }
-        double acquiredGPA = (currentGPA * moduleCreditsTaken + mockGPA *
+        double acquiredGPA = (currentGPA * moduleCreditsCountedToGPA + mockGPA *
                 (4 * (upperBoundGradeNeeded + lowerBoundGradeNeeded))) / totalModuleCreditsCountedToGPA;
         printGradeExpectations(desiredGPA, acquiredGPA, upperBoundGradeNeeded,
                 upperBound, lowerBoundGradeNeeded, lowerBound);
