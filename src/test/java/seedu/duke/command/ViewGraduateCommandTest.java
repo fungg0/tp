46cf896f31310a229f5f0871ae187129d697a290
fungg0 <fungfunwenwu@gmail.com>
2024-04-12 02:24:35 +0800

    Amended JUnit testing to accomodate the change in Module Constructor

--- src/test/java/seedu/duke/command/ViewGraduateCommandTest.java
+++ src/test/java/seedu/duke/command/ViewGraduateCommandTest.java
@@ -26,7 +26,7 @@ class ViewGraduateCommandTest {
         for (CEGModules cegModules : CEGModules.values()) {
             boolean isInternship = cegModules.name().equals("EG3611A") || cegModules.name().equals("CP3880");
             if (!isInternship) {
-                moduleList.addModule(new Module(cegModules.name(), 4, 1, ""));
+                moduleList.addModule(new Module(cegModules.name(), 4, 1, "", true));
                 graduateModuleList.remove(cegModules.name());
                 assertEquals(graduateModuleList, moduleList.getModulesToComplete());
             }
@@ -35,16 +35,16 @@ class ViewGraduateCommandTest {
 
     @Test
     public void viewGraduateCommandNonCEGModulesTest() {
-        moduleList.addModule(new Module("AC5001", 4, 1, ""));
-        moduleList.addModule(new Module("GEC1005", 4, 1, ""));
-        moduleList.addModule(new Module("MA2101", 4, 1, ""));
-        moduleList.addModule(new Module("LC6009GRSII", 4, 1, ""));
+        moduleList.addModule(new Module("AC5001", 4, 1, "", true));
+        moduleList.addModule(new Module("GEC1005", 4, 1, "", true));
+        moduleList.addModule(new Module("MA2101", 4, 1, "", true));
+        moduleList.addModule(new Module("LC6009GRSII", 4, 1, "", false));
         assertEquals(graduateModuleList, moduleList.getModulesToComplete());
     }
 
     @Test
     public void viewGraduateCommandEquivalentModulesTest1() {
-        moduleList.addModule(new Module("EG3611A", 10, 1, ""));
+        moduleList.addModule(new Module("EG3611A", 10, 1, "", true));
         graduateModuleList.remove("EG3611A");
         graduateModuleList.remove("CP3880");
         assertEquals(graduateModuleList, moduleList.getModulesToComplete());
@@ -52,7 +52,7 @@ class ViewGraduateCommandTest {
 
     @Test
     public void viewGraduateCommandEquivalentModulesTest2() {
-        moduleList.addModule(new Module("CP3880", 12, 1, ""));
+        moduleList.addModule(new Module("CP3880", 12, 1, "", true));
         graduateModuleList.remove("EG3611A");
         graduateModuleList.remove("CP3880");
         assertEquals(graduateModuleList, moduleList.getModulesToComplete());
